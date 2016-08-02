eb4c9e3d4372858de799fa859a06bba7a9899d6c
myflwq <1084091692@qq.com>
2016-08-02 15:36:37 +0800

    api.github

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import axios from 'axios';
+
+
 
 class App extends React.Component {
+  getUserInfo(username){
+    return axios.get(`https://api.github.com/users/myflwq`)
+    .then((res)=>(
+        {
+          gitInfo: res.data
+        }
+      )
+    )
+  }
+  constructor(){
+    super();
+    this.state={
+      info:{}
+    }
+  }
+  componentDidMount(){
+    this.getUserInfo().then((x)=>{
+      console.log(x.gitInfo);
+      this.setState({
+        info:x.gitInfo
+      })
+    })
+  }
   render () {
     return(
-      <h1>脚手架工具</h1>
+      <h1>{this.state.info.followers}</h1>
     )
   }
 }
