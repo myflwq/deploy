02800f2fa77c45e5c0614a340f362f4312607a28
myflwq <1084091692@qq.com>
2016-08-02 16:21:11 +0800

    loding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import axios from 'axios';
-
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import CircularProgress from 'material-ui/CircularProgress';
 
 
 class App extends React.Component {
+  getChildContext() {
+     return {muiTheme: getMuiTheme()};
+   }
   getUserInfo(username){
     return axios.get(`https://api.github.com/users/myflwq`)
     .then((res)=>(
@@ -16,22 +20,34 @@ class App extends React.Component {
   constructor(){
     super();
     this.state={
-      info:{}
+      info:{},
+      wait:true
     }
   }
   componentDidMount(){
     this.getUserInfo().then((x)=>{
       console.log(x.gitInfo);
       this.setState({
-        info:x.gitInfo
+        info:x.gitInfo,
+        wait:false
       })
     })
   }
   render () {
     return(
-      <h1>{this.state.info.followers}</h1>
+      <div>
+        {
+          this.state.wait ? <CircularProgress size={1.5} /> :
+          <div>
+            <img src={this.state.info.avatar_url} />
+          </div>
+        }
+      </div>
+
     )
   }
 }
-
+App.childContextTypes = {
+ muiTheme: React.PropTypes.object.isRequired,
+};
 export default App;
